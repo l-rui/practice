a097309489be1ef9ec928129b9655e5b4fad42e5
lr1988 <lr_19880820@163.com>
2017-04-09 21:58:43 +0800

    列表翻转，修改了实现方式，添加了一些注释

--- src/practice/algorithm/ReverseLinkedList.java
+++ src/practice/algorithm/ReverseLinkedList.java
@@ -11,17 +11,41 @@ public class ReverseLinkedList {
         if(head==null || head.next==null) {
             return head;
         }
-        ListNode pre = head;
-        ListNode p = head.next;
-        pre.next = null;
-        ListNode nxt;
-        while(p!=null) {
-            nxt = p.next;
-            p.next = pre;
-            pre = p;
-            p = nxt;
+        // 当前节点的前一个节点，初始为null
+        ListNode pre;
+        // 当前节点
+        ListNode cur = head;
+        // 当前节点的后一个节点
+        ListNode nxt = head.next;
+        // 在翻转之前，首先将第一个节点的next值设置为null，否则将形成环状链表
+        cur.next = null;
+        while (nxt != null) {
+            // 首先将后一个nxt节点移动到pre
+            pre = nxt;
+            // 将后nxt节点索引再往后移动一位
+            nxt = nxt.next;
+            // 将pre的next索引设置为cur
+            pre.next = cur;
+            // 完成后将cur索引前移一位
+            cur = pre;
         }
-        return pre;
+        return cur;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        ReverseLinkedList rll = new ReverseLinkedList();
+        ListNode l = rll.reverseList(head);
+        ListNode temp = l;
+        while (null != temp) {
+            System.out.print(temp.val);
+            temp = temp.next;
+        }
+
     }
 
 }
